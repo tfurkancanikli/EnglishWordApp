63a7275e63c74c8e091ec2e37c6367dea2fa041f
Mahmut Can Conger <mahmutconger@gmail.com>
2025-04-19 13:07:43 +0300

    kayıt ve giriş sayfalarının tasarımları yapıldı.

---
+++ app/src/main/java/com/anlarsinsoftware/englishwordsapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.anlarsinsoftware.englishwordsapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+
+    }
+    fun signUpClick(view : View){
+        intent = Intent(this,RegisterActivity :: class.java)
+        startActivity(intent)
+    }
+
+    fun signInClick(view : View){
+        intent = Intent(this,SignInActivity :: class.java)
+        startActivity(intent)
+    }
+
+}
---
+++ app/src/main/java/com/anlarsinsoftware/englishwordsapp/SignInActivity.kt
@@ -0,0 +1,26 @@
+package com.anlarsinsoftware.englishwordsapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_in)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun backTM(view : View){
+        intent = Intent(this,MainActivity:: class.java)
+        startActivity(intent)
+    }
+}
