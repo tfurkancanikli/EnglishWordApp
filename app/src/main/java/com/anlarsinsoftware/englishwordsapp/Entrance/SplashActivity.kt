c5e5e0a89d73ea605a8d909176c26654bb3693e5
Mahmut Can Conger <mahmutconger@gmail.com>
2025-05-19 00:20:47 +0300

    Splash Screen eklendi

---
+++ app/src/main/java/com/anlarsinsoftware/englishwordsapp/Entrance/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.anlarsinsoftware.englishwordsapp.Entrance
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.anlarsinsoftware.englishwordsapp.R
+import com.anlarsinsoftware.englishwordsapp.Util.bagla
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+        bagla(SignInActivity::class.java,true)},2525)
+
+    }
+}
